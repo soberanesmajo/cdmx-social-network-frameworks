4848b769146e7122b063caac19831ecb86941d7d
soberanesmajo <soberanesmajo@gmail.com>
2018-10-02 22:44:21 -0500

    agrega componente Posts con funcionalidad en Firebase

--- social-network/src/components/navbar/Navbar.js
+++ social-network/src/components/navbar/Navbar.js
@@ -1,11 +1,57 @@
 import React from 'react';
 import { Navbar, NavItem} from 'react-materialize';
 import './navbar.css';
+import firebase from 'firebase';
+import { Redirect } from 'react-router-dom';
+import Login from '../login/Login';
+
+// class navbarComp extends Component {
+//   constructor () {
+//     super ();
+//     this.state = {user};
+
+//   this.handleLogout = this.handleLogout.bind(this);
+
+//   }
+
+//   handleLogout () {
+//     firebase.auth().signOut()
+//     .then(result => <Redirect to = '/'/>)                        // console.log(`${result.user.email} ha cerrado sesión`)
+//     .catch(error => console.log(`Error ${error.code}: ${error.message}`));
+//   }
+
+//   render () {
+//     return (
+//       <Navbar brand='InsideOut' right className = "navbar">
+//         {/* <img src = {this.state.user.photoURl} alt = {this.user.displayName}/> */}
+//         <NavItem onClick={() => console.log('test click')}>Perfil</NavItem>
+//         <NavItem onClick = {this.handleLogout}>Cerrar Sesión</NavItem>
+//       </Navbar>
+//     )
+//   }
+// }
+
+//   export default navbarComp;
+
+const handleLogout = () => {
+  console.log('holi');
+  firebase.auth().signOut()
+  .then(result => {
+    <Redirect to = '/' />
+    console.log(`Se ha cerrado sesión`)})
+  .catch(error => console.log(`Error ${error.code}: ${error.message}`));
+
+
+}
+  
+
+
 
 const navbarComp = () => 
   <Navbar brand='InsideOut' right className = "navbar">
+    {/* <img src = {this.state.user.photoURl} alt = {this.user.displayName}/> */}
     <NavItem onClick={() => console.log('test click')}>Perfil</NavItem>
-    <NavItem href='components.html'>Cerrar Sesión</NavItem>
+    <NavItem onClick = {handleLogout}>Cerrar Sesión</NavItem>
   </Navbar>
 
 export default navbarComp;
--- social-network/src/views/App.js
+++ social-network/src/views/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-// import Navigation from '../components/navigation/Navigation';
 import Login from '../components/login/Login';
 import Signup from '../components/signup/Signup';
 import Home from '../components/home/Home';
 import Profile from '../components/profile/Profile';
-import Error from '../components/error/Error';
+// import Error from '../components/error/Error';
 
 const App = () =>
-      // <Router path = {process.env.PUBLIC_URL}>
-      <Router path={process.env.PUBLIC_URL + '/'}>
+      <Router basename={process.env.PUBLIC_URL}>
         <div>
 
         <Switch>
@@ -34,10 +32,10 @@ const App = () =>
             component = {Profile}
           />
 
-          <Route
+          {/* <Route
             
             component = {Error}
-          />
+          /> */}
 
         
         </Switch>        

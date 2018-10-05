1b5f7006112b79d58207319f20ee05e472be7b1c
soberanesmajo <soberanesmajo@gmail.com>
2018-10-04 23:40:43 -0500

    modifica estilos

--- social-network/src/components/card/Card.js
+++ social-network/src/components/card/Card.js
@@ -6,10 +6,6 @@ import {
   Col,
   CardPanel
 } from 'react-materialize';
-// import firebase from 'firebase';
-
-//import db from '../../index';
-// const db = firebase.firestore();
 
 
 class Cardprint extends Component {
@@ -19,7 +15,6 @@ class Cardprint extends Component {
       posts: []
     }
   }
-
 
   componentDidMount() {
 
@@ -40,7 +35,6 @@ class Cardprint extends Component {
     }
 
     render() {
-      //const {posts} = this.state;
         return (
           <div>
           {this.state.posts.reverse().map(post => 
@@ -56,64 +50,7 @@ class Cardprint extends Component {
             )}
             </div>
         )
-    //       {this.state.posts.map(post => { 
-    //         <div className = 'container'>
-    //           <Row>
-    //             <Col s={12} m={5}>
-    //               <CardPanel className="teal lighten-4 black-text">
-    //               <span>{post}</span>
-    //                 </CardPanel>
-    //             </Col>
-    //           </Row>
-    //         </div>
-    //       })
-    //     )
-    //     }
-    
-    //   // return ( 
-    //   //   <div className = 'container'>
-    //   //     <Row>
-    //   //       <Col s={12} m={5}>
-    //   //         <CardPanel className="teal lighten-4 black-text">
-    //   //         <span>comentarios</span>
-    //   //           </CardPanel>
-    //   //       </Col>
-    //   //     </Row>
-    //   //   </div>
-    //   // )
-    // }
   }
 }
-
-
-  // render () {
-  //   // const {posts} = this.state;
-  //   return (
-  //     <div className = 'container'>
-  //       <Row>
-  //         <Col s={12} m={5}>
-  //           <CardPanel className="teal lighten-4 black-text">
-  //             <span>comentarios</span>
-  //             </CardPanel>
-  //         </Col>
-  //       </Row>
-  //     </div>
-  //   )
-  // }
-
-
-
-
-
-  // const cardPrint = () =>
-  //   <div className = 'container'>
-  //     <Row>
-  //       <Col s={12} m={5}>
-  //         <CardPanel className="teal lighten-4 black-text">
-  //           <span>Comentarios</span>
-  //           </CardPanel>
-  //       </Col>
-  //     </Row>
-  //   </div>
 
   export default Cardprint;
